2e1d1041c199d9fd55495a2d38773a262e9d7b69
lrorgan <lrorgan@sep.com>
2014-08-02 18:06:34 -0400

    labs link

---
+++ color-exploder/Assets/Scripts/LabsLink.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System;
+
+public class LabsLink : MonoBehaviour {
+
+	private static Uri url = new Uri(@"http://www.sep.com/labs/");
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown () {
+		Application.ExternalCall("open2", url, 1);
+	}
+}
---
+++ color-exploder/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MainMenu : MonoBehaviour {
+
+	private static Uri url = new Uri(@"http://www.sep.com/labs/");
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
