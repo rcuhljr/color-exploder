7232b465500e61b7d14dcf6f33770bcc6d515db3
unknown <egwilliamson@Shine.net.sep.com>
2014-08-03 14:57:03 -0400

    Fixed special bomb sound effect

--- color-exploder/Assets/Scripts/ColorBombScript.cs
+++ color-exploder/Assets/Scripts/ColorBombScript.cs
@@ -1,11 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Timers;
 
 public class ColorBombScript : MonoBehaviour {
 
   public int BombsLeft = 1;
   public BackgroundScript bgScript;
   public SoundScript sound;
+  private Timer delayTimer;
+  private bool dropBomb = false;
+
+  public void Start () {
+    delayTimer = new Timer (750);
+    delayTimer.Elapsed += dropReady;
+  }
+
+  void dropReady (object sender, ElapsedEventArgs e)
+  {
+    dropBomb = true;
+  }
 
   public void DropBomb(){
     if (BombsLeft > 0) {
@@ -13,9 +26,17 @@ public class ColorBombScript : MonoBehaviour {
       {
         sound.Play(SoundScript.SoundList.Bombs);
       }
+      delayTimer.Start();
       BombsLeft -= 1;
-      bgScript.ChangeColor(ColorUtils.Colors.player);
       GuiScript.UpdateBomb(BombsLeft);
+    }
+  }
+
+  public void Update() {
+    if (dropBomb) {
+      dropBomb = false;
+      bgScript.ChangeColor (ColorUtils.Colors.player);
+      delayTimer.Stop ();
     }
   }
 }
