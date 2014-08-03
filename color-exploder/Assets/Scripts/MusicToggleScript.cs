1d6e2f56282db443f90151416cfe2b0739799329
unknown <egwilliamson@Shine.net.sep.com>
2014-08-03 11:13:43 -0400

    Background music toggle effect

---
+++ color-exploder/Assets/Scripts/MusicToggleScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicToggleScript : MonoBehaviour {
+
+	public SoundScript sound;
+	public bool lastKnownState;
+
+	// Use this for initialization
+	void Start () {
+		lastKnownState = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool currentState = Options.Entries ["Music"];
+		if (currentState != lastKnownState) {
+			if (sound != null) {
+				sound.MuteMusic(!currentState);
+			}
+      lastKnownState = currentState;
+		}
+	}
+}
--- color-exploder/Assets/Scripts/SoundScript.cs
+++ color-exploder/Assets/Scripts/SoundScript.cs
@@ -16,32 +16,91 @@ public class SoundScript : MonoBehaviour {
 		GameOvers,
 		Lasers,
 		Transitions
-	}	
+	}
 
 	public void Play(SoundList sound)
 	{
 		if (Options.Entries["SoundEffects"]) {
 			switch(sound)
 			{
-				case SoundList.Bombs:
-					audio.PlayOneShot(bombSound, 0.7F);
-					break;
-				case SoundList.Explosions:
-					audio.PlayOneShot(explosionSound, 0.7F);
-					break;
-				case SoundList.GameOvers:
-					audio.PlayOneShot(gameOverSound, 0.7F);
-					break;
-				case SoundList.Lasers:
-					audio.PlayOneShot(laserSound, 0.7F);
-					break;
-				case SoundList.Transitions:
-					audio.PlayOneShot(transitionSound, 0.7F);
-					break;
+			case SoundList.Bombs:
+				audio.PlayOneShot(bombSound, 0.7F);
+				break;
+			case SoundList.Explosions:
+				audio.PlayOneShot(explosionSound, 0.7F);
+				break;
+			case SoundList.GameOvers:
+				audio.PlayOneShot(gameOverSound, 1F);
+				break;
+			case SoundList.Lasers:
+				audio.PlayOneShot(laserSound, 0.7F);
+				break;
+			case SoundList.Transitions:
+				audio.PlayOneShot(transitionSound, 0.7F);
+				break;
 			}
 		}
 	}
+	
+	public AudioClip menuSound;
+	public AudioClip randomSound;
+	public AudioClip blueSound;
+	public AudioClip greenSound;
+	public AudioClip redSound;
+	public AudioClip bossSound;
 
-	//public void Start(){}
-	//public void Update(){}
+	public enum BackgroundSound
+	{
+		MenuSong,
+		RandomSong,
+		BlueSong,
+		GreenSong,
+		RedSong,
+		BossSong
+	}
+
+	public void PlayMusic(BackgroundSound song)
+	{
+		if (Options.Entries["Music"]) {
+			switch(song)
+			{
+			case BackgroundSound.MenuSong:
+				audio.clip = menuSound;
+				audio.Play();
+				break;
+			case BackgroundSound.RandomSong:
+				audio.clip = randomSound;
+				audio.Play();
+				break;
+			case BackgroundSound.BlueSong:
+				audio.clip = blueSound;
+				audio.Play();
+				break;
+			case BackgroundSound.GreenSong:
+				audio.clip = greenSound;
+				audio.Play();
+				break;
+			case BackgroundSound.RedSong:
+				audio.clip = redSound;
+				audio.Play();
+				break;
+			case BackgroundSound.BossSong:
+				audio.clip = bossSound;
+				audio.Play();
+				break;
+			}
+			audio.loop = true;
+		}
+	}
+
+	public void StopMusic()
+	{
+		if (!Options.Entries["Music"]) {
+			audio.Stop ();
+		}
+	}
+
+	public void MuteMusic(bool state) {
+		audio.mute = state;
+	}
 }
