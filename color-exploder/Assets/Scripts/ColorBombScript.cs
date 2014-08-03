5c4c647160de1a13c29c07f50f465ba690c8a976
unknown <egwilliamson@Shine.net.sep.com>
2014-08-03 14:47:13 -0400

    Added sounds to special bombs

--- color-exploder/Assets/Scripts/ColorBombScript.cs
+++ color-exploder/Assets/Scripts/ColorBombScript.cs
@@ -5,9 +5,14 @@ public class ColorBombScript : MonoBehaviour {
 
   public int BombsLeft = 1;
   public BackgroundScript bgScript;
+  public SoundScript sound;
 
   public void DropBomb(){
     if (BombsLeft > 0) {
+      if(sound != null)
+      {
+        sound.Play(SoundScript.SoundList.Bombs);
+      }
       BombsLeft -= 1;
       bgScript.ChangeColor(ColorUtils.Colors.player);
       GuiScript.UpdateBomb(BombsLeft);
