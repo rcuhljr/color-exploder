8015210f3648acf0a75412e8db989f6159c70de3
Robert C. Uhl <rcuhl@sep.com>
2014-08-03 12:51:59 -0400

    warning

--- color-exploder/Assets/Scripts/SpawnEnemiesScript.cs
+++ color-exploder/Assets/Scripts/SpawnEnemiesScript.cs
@@ -18,19 +18,10 @@ public class SpawnEnemiesScript : MonoBehaviour
   /// Projectile prefab for shooting
   /// </summary>
   public Transform enemyPrefab;
-
   public BackgroundScript bgScript;
-
-    public Transform asteroidPrefab;
-
-		public SoundScript sound;
-
-        /// <summary>
-        /// Cooldown in seconds between two enemies
-        /// </summary>
-        private bool readyToSpawn = false;
-        public System.Random randomzier = new System.Random ();
-
+  public Transform asteroidPrefab;
+  public SoundScript sound;
+  public System.Random randomzier = new System.Random ();
 
   void Start ()
   {
@@ -40,70 +31,66 @@ public class SpawnEnemiesScript : MonoBehaviour
   {
   }
 
-  public void SpawnRandomEnemy(){
-      //Drop the enemy into one of thirteen "slots"
-    var position = new Vector3 ((float)((randomzier.Next(0,13)) - 6), 5, 1);
+  public void SpawnRandomEnemy ()
+  {
+    //Drop the enemy into one of thirteen "slots"
+    var position = new Vector3 ((float)((randomzier.Next (0, 13)) - 6), 5, 1);
     var color = (Colors)(randomzier.Next () % 3);
     SpawnEnemy (position, color, (randomzier.Next () % 10) == 1, false);   
-    }
+  }
 
-    public void SpawnAsteroid(Vector3 position) {
-      var enemy = Instantiate (asteroidPrefab) as Transform;
-      enemy.position = position;
-		var collider = enemy.GetComponentsInChildren<AsteroidCollision>().First();
-		collider.sound = sound;
-	}
-	
-	public void SpawnEnemy (Vector3 position, Colors color, bool isShielded, bool rotates)
-	  {
+  public void SpawnAsteroid (Vector3 position)
+  {
+    var enemy = Instantiate (asteroidPrefab) as Transform;
+    enemy.position = position;
+    var collider = enemy.GetComponentsInChildren<AsteroidCollision> ().First ();
+    collider.sound = sound;
+  }
+  
+  public void SpawnEnemy (Vector3 position, Colors color, bool isShielded, bool rotates)
+  {
     if (color == bgScript.color) {
       //Debug.Log("attempt to spawn blocked color:"+color+":"+bgScript.color);
       //#TODO Fix with better pick
-      color = ColorUtils.GetRandomColorForBackground(color, randomzier);
+      color = ColorUtils.GetRandomColorForBackground (color, randomzier);
     }
-						var enemy = Instantiate (enemyPrefab) as Transform;
+    var enemy = Instantiate (enemyPrefab) as Transform;
     enemy.tag = "enemy_ship";
-      enemy.position = position;
-            List<Transform> disabledCannons = new List<Transform> ();
+    enemy.position = position;
+    List<Transform> disabledCannons = new List<Transform> ();
 
     var enemyMovement = enemy.GetComponent<StraightEnemyScript> ();
     if (enemyMovement != null) {
       enemyMovement.rotates = rotates;
     }
 
-		for(int i=0; i<enemy.childCount; i++)
-		{
-			var child = enemy.GetChild(i);
+    for (int i=0; i<enemy.childCount; i++) {
+      var child = enemy.GetChild (i);
 
 
-			var renderer = child.GetComponent<SpriteRenderer> ();
-			    renderer.color = ColorUtils.ConvertToColor (color);
+      var renderer = child.GetComponent<SpriteRenderer> ();
+      renderer.color = ColorUtils.ConvertToColor (color);
 
-			if(!renderer.enabled )
-			{
-				if(randomzier.Next()%3 == 0)
-				{
-					renderer.enabled = true;
-			}
-				else
-				{
-					disabledCannons.Add(child);
-				}
-			}		        
-		}
-		if (disabledCannons.Count>7) 
-		{
-			var luckyCannon = randomzier.Next()%7;
-			disabledCannons[luckyCannon].GetComponent<SpriteRenderer>().enabled = true;
-			disabledCannons.RemoveAt(luckyCannon);
-		}
+      if (!renderer.enabled) {
+        if (randomzier.Next () % 3 == 0) {
+          renderer.enabled = true;
+        } else {
+          disabledCannons.Add (child);
+        }
+      }           
+    }
+    if (disabledCannons.Count > 7) {
+      var luckyCannon = randomzier.Next () % 7;
+      disabledCannons [luckyCannon].GetComponent<SpriteRenderer> ().enabled = true;
+      disabledCannons.RemoveAt (luckyCannon);
+    }
     foreach (var collider in
               (enemy as Transform).GetComponentsInChildren<EnemyCollision> ()) {
       collider.EnemyColor = color;
-          collider.isShielded = isShielded;
-		  collider.Enemy = (enemy as Transform).gameObject;
-		  collider.DisabledCannons = disabledCannons;
-			collider.sound = sound;
+      collider.isShielded = isShielded;
+      collider.Enemy = (enemy as Transform).gameObject;
+      collider.DisabledCannons = disabledCannons;
+      collider.sound = sound;
     }
 
     if (isShielded) {
